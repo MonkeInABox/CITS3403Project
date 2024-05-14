a99c9eb27a39e150b746ecc1663ac7b32d6e86f5
Skoll-8 <dylanarto@live.com.au>
2024-05-14 16:45:14 +0800

    Filter not functioning, committed for purpose of fixing

--- app/main/forms.py
+++ app/main/forms.py
@@ -1,5 +1,5 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField, TextAreaField
+from wtforms import StringField, SubmitField, TextAreaField, SelectField
 from wtforms.validators import Length, DataRequired
 
 class EditProfileForm(FlaskForm):
@@ -12,3 +12,15 @@ class SearchForm(FlaskForm):
 
 class Delete(FlaskForm):
     submit = SubmitField("Delete")
+
+class FilterForm(FlaskForm):
+    filter = SelectField("Filter Posts: ",
+                            choices=[
+                               ('nwst', 'Newest'),   # DO NOT GO ABOVE 4 CHARS FOR DATABASE VALUE
+                               ('ldst', 'Oldest'),  # DATABASE CAN ONLY HANDLE 4 CHARS!!!
+                               ('mslk', 'Most Liked'),
+                               ('lslk', 'Least Liked'),
+                               ('mscm', 'Most Commented')
+                           ],
+                           default = 'nwst')
+    submit = SubmitField("Submit Filter")
