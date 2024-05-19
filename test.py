5a092e54fe4fad8001aad4f8d6c53b9ce9c1671c
Skoll-8 <dylanarto@live.com.au>
2024-05-19 14:31:36 +0800

    Tests updated (Not working)

--- test.py
+++ test.py
@@ -1,29 +1,31 @@
 #!/usr/bin/env python
 import unittest
-from app import create_app, db
+from app import create_app, db, current_app
 from app.models import User, Post, Comment
 from config import Config
 from app.posts.forms import PostNewPost
-from selenium import webdriver
-from selenium.webdriver.chrome.service import Service as ChromeService
-from webdriver_manager.chrome import ChromeDriverManager
+#from selenium import webdriver, multiprocessing
+#from selenium.webdriver.chrome.service import Service as ChromeService
+#from webdriver_manager.chrome import ChromeDriverManager
 
 
 class TestConfig(Config):
     TESTING = True
     SQLALCHEMY_DATABASE_URI = 'sqlite://'
+    WTF_CSRF_ENABLED = False  # Disable CSRF for testing
+    RECAPTCHA_USE_SSL = False
+    RECAPTCHA_PUBLIC_KEY = 'test'
+    RECAPTCHA_PRIVATE_KEY = 'test'
+    RECAPTCHA_OPTIONS = {'theme': 'white'}
+
 
 class UserModelCase(unittest.TestCase):
     def setUp(self):
         self.app = create_app(TestConfig)
         self.app_context = self.app.app_context()
         self.app_context.push()
-        db.create_all()
-
-    def tearDown(self):
-        db.session.remove()
-        db.drop_all()
-        self.app_context.pop()
+        with self.app_context:
+            db.create_all()
 
     def test_password_hashing(self):
         u = User(username='susan', email='susan@example.com')
@@ -38,53 +40,46 @@ class UserModelCase(unittest.TestCase):
                                          '?d=identicon&s=128'))
     
     def test_post(self):
-        u = User(id = 24)
-        post_body = "I love Harrison Ford, what's a good movie with him in it?"
-        self.app.post('/newpost', body = post_body, user_id = u.id)
-        post = db.select(Post).filter_by(user_id=u.id)
-        self.assertTrue(post is not None)
-    
-    def test_comment(self):
-        post_u = User(id = 24)
-        comment_u = User(id = 38)
-        post_id = 4
-        post_body = "I love Harrison Ford, what's a good movie with him in it?"
-        comment_body = "OMG, you HAVE to watch Blade Runner!"
-        self.app.post('/newpost', body = post_body, user_id = post_u.id, id = post_id)
-        self.app.post('/submit_comment', body = comment_body, author_id = comment_u.id, post_id = post_id)
-        comment = db.select(Post).join(Comment).group_by(Post.id).filter_by(author_id=comment_u.id)
-        self.assertTrue(comment is not None)
+        post_data = {
+            'body': "I love Harrison Ford, what's a good movie with him in it?",
+            'category': 'film',
+            'submit': 'Submit Post'
+        }
+        current_user = User(id = 1, username = "test", email = "ford@gmail.com")
+        result = self.app.post('/newpost', data = post_data, current_user=current_user)
+        with self.app_context:
+            post = Post.query.filter_by(category='film').first()
+            self.assertEqual(post.body == "I love Harrison Ford, what's a good movie with him in it?")
 
-    def test_categories(self):
-        post_u = User(id = 24)
-        self.app.post('/newpost', body = "any movies with Harrison Ford?", user_id = post_u.id, category = "film")
-        self.app.post('/newpost', body = "any video games with Harrison Ford?", user_id = post_u.id, category = "vdga")
-        self.app.post('/newpost', body = "any books with Harrison Ford?", user_id = post_u.id, category = "book")
-        self.app.post('/newpost', body = "any music with Harrison Ford?", user_id = post_u.id, category = "musc")
-        self.app.post('/newpost', body = "any tv shows with Harrison Ford?", user_id = post_u.id, category = "tvsh")
-        print(self.app.get('categories/film'))
-
-localHost = "http://localhost:5000"
-
-class SeleniumTests(unittest.TestCase):
-    def setUp(self):
-        self.app = create_app(TestConfig)
-        self.app_context = self.app.app_context()
-        self.app_context.push()
-        db.create_all()
-
-        self.server_thread = multiprocessing.Process(target=self.testApp.run)
-        self.server_thread.start()
-
-        self.driver = webdriver.Chrome()
-        self.driver.get(localHost)
 
     def tearDown(self):
-        self.server_thread.terminate()
-        self.driver.close()
         db.session.remove()
         db.drop_all()
         self.app_context.pop()
+
+localHost = "http://localhost:5000"
+
+#class SeleniumTests(unittest.TestCase):
+#    def setUp(self):
+#        self.app = create_app(TestConfig)
+#        self.app_context = self.app.app_context()
+#        self.app_context.push()
+#        db.create_all()
+
+#        self.server_thread = multiprocessing.Process(target=self.testApp.run)
+#        self.server_thread.start()
+
+#        self.driver = webdriver.Chrome()
+#        self.driver.get(localHost)
+
+#    def tearDown(self):
+#        self.server_thread.terminate()
+#        self.driver.close()
+#        db.session.remove()
+#        db.drop_all()
+#        self.app_context.pop()
+    
+
 
         
 if __name__ == '__main__':
