f1bdc5834059275484e63dfa27b215eac8894e23
m3ganz <164032765+m3ganz@users.noreply.github.com>
2024-05-19 15:03:28 +0800

    Update test.py
    added test for user registration?

--- test.py
+++ test.py
@@ -1,9 +1,9 @@
 #!/usr/bin/env python
 import unittest
-from app import create_app, db
-from app.models import User, Post
-from config import Config
 from flask import url_for
+from app import create_app, db
+from app.models import User,Post
+from config import Config
 
 class TestConfig(Config):
     TESTING = True
@@ -11,6 +11,9 @@ class TestConfig(Config):
     SERVER_NAME = 'localhost'  # Setting SERVER_NAME for URL building
     APPLICATION_ROOT = '/'     # Application root
     PREFERRED_URL_SCHEME = 'http'  # Preferred URL scheme
+    WTF_CSRF_ENABLED = False  # Disable CSRF protection for testing
+
+
 
 class UserModelCase(unittest.TestCase):
     def setUp(self):
@@ -19,12 +22,12 @@ class UserModelCase(unittest.TestCase):
         self.app_context.push()
         db.create_all()
         self.client = self.app.test_client()
-
         self.user = User(username='testuser', email='test@example.com')
         self.user.set_password('password')
         db.session.add(self.user)
         db.session.commit()
-        self.login()
+        if self._testMethodName not in ['test_user_registration']:
+            self.login()
 
     def tearDown(self):
         db.session.remove()
@@ -32,10 +35,10 @@ class UserModelCase(unittest.TestCase):
         self.app_context.pop()
 
     def login(self):
-        self.client.post(url_for('auth.login'), data={
+        response = self.client.post(url_for('auth.login'), data={
             'username': 'testuser',
             'password': 'password'
-        })
+        }, follow_redirects=True)
 
     def test_password_hashing(self):
         u = User(username='testuser', email='testuser@example.com')
@@ -48,6 +51,18 @@ class UserModelCase(unittest.TestCase):
         self.assertEqual(u.avatar(128), ('https://www.gravatar.com/avatar/'
                                          'd4c74594d841139328695756648b6bd6'
                                          '?d=identicon&s=128'))
+                                         
+    def test_user_registration(self):
+        response = self.client.post('/register', data={
+            'username': 'newuser',
+            'email': 'newuser@example.com',
+            'password': 'password',
+            'password2': 'password'
+        },follow_redirects=True)
+        user = User.query.filter_by(username='newuser').first()
+        self.assertIsNotNone(user) 
+        self.assertEqual(user.username, 'newuser')
+
 
         
 if __name__ == '__main__':
